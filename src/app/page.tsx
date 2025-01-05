0f493b833d5475637c7921df4d66451420c13a9b
KhoeTrinh <kelvintrinh09@gmail.com>
2025-01-05 15:23:38 +0700

    feat: implement sign-in and sign-up pages; add utility functions and update layout components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
-  return (
-    <div>
-      <h1 className="">StoreIt - The only storage solution you need.</h1>
-    </div>
-  );
+    return (
+        <div className='flex-center h-screen'>
+            <h1 className='h1'>
+                StoreIt - The only storage solution you need.
+            </h1>
+        </div>
+    );
 }
